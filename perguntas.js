8b0620e2be11c1158d29e4f2932bf7dcf0f67f38
juliavlourenco <julia.vertuan.lourenco@escola.pr.gov.br>
2025-08-14 14:40:32 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,37 @@
+Criacartao(
+'biologia',
+'Qual é o menor orgão do corpo humano?',
+ 'glândula pineal'
+
+)
+Criacartao(
+  'biologia',
+  'Qual é o maior órgão do corpo humano?',
+  'pele'
+)
+
+Criacartao(
+  'biologia',
+  'Qual é o nome da célula responsável pela transmissão de impulsos nervosos?',
+  'neurônio'
+)
+
+Criacartao(
+  'biologia',
+  'Qual é o nome da molécula que carrega o código genético?',
+  'DNA'
+)
+
+Criacartao(
+  'biologia',
+  'Como é chamada a unidade funcional dos rins?',
+  'néfron'
+)
+
+Criacartao(
+  'biologia',
+  'Qual é o processo pelo qual as plantas produzem seu próprio alimento?',
+  'fotossíntese'
+)
+
+
